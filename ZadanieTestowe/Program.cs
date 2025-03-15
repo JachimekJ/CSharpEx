b36d3041495026942bf5b7778910052d611edda9
JachimekJ <jachimekkuba@gmail.com>
2025-03-15 15:33:29 +0100

    Dodano pierwsze zadanie w C#

---
+++ ZadanieTestowe/Program.cs
@@ -0,0 +1,13 @@
+﻿//Test tworzenia i inicjalizacji projektu C#//
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Witaj w zadaniach C#!");
+
+    }
+   
+}
