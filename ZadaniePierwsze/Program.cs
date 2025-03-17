ec2e36908a9252ddb6a20a7d0ee6f0fa67af2a00
JachimekJ <jachimekkuba@gmail.com>
2025-03-17 22:50:32 +0100

    Napisano prosty program sumujący

--- ZadaniePierwsze/Program.cs
+++ ZadaniePierwsze/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+//Program, obliczający sumę trzech podanych liczb całkowitych//
+class Program
+
+ {
+    static void Main() 
+    {
+
+        Console.WriteLine("Podaj pierwszą liczbę;");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj drugą liczbę;");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj trzecią liczbę;");
+        int c = int.Parse(Console.ReadLine());
+
+        int Suma = a + b + c;
+
+        
+        Console.WriteLine("Suma podanych liczb to: " + Suma);
+        
+        
+    }
+ }
