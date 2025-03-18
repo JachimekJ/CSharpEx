599467c4ef0ae06762ada6f36f2ee3a95d34bce7
JachimekJ <jachimekkuba@gmail.com>
2025-03-18 13:53:11 +0100

    Ukończono ZadanieCzwarte

--- ZadanieCzwarte/Program.cs
+++ ZadanieCzwarte/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//GRA W ZGADYWANIE LICZBY PRZEZ KOMPUTER
+//KOMPUTER LOSUJE LICZBĘ Z ZAKRESU 1 - 10
+//USER STARA SIĘ ODGADNĄĆ LICZBĘ
+//JEŚLI PODANA LICZBA JEST MNIEJSZA LUB WIĘKSZA NIŻ WYLOSOWANA - WYŚWIETL KOMUNIKAT
+//KIEDY USER ODGADNIE LICZBĘ - WYŚWIETL KOMUNIKAT O WYGRANEJ
+
+class Program
+{
+
+    static void Main(String[] args)
+    {
+        Random rnd = new Random();
+        int wylosowanaLiczba = rnd.Next(1, 11);
+        int mojaLiczba;
+        do
+        {
+           Console.Write("Podaj liczbę od 1 do 10: ");
+            mojaLiczba = int.Parse(Console.ReadLine());
+            if(mojaLiczba > wylosowanaLiczba)
+            {
+                Console.WriteLine("Podana liczba jest za duża");
+                
+            }
+            else if(mojaLiczba < wylosowanaLiczba)
+            {
+                Console.WriteLine("Podana liczba jest za mała");
+            }
+            else
+            {
+                Console.WriteLine("Brawo! Odgadłeś liczbę!");
+                break;
+            }
+        } while (wylosowanaLiczba != mojaLiczba);
+    }
+
+
+
+
+
+
+}
